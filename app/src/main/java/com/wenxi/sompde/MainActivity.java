2b81ebed4fb0809ccfa1bd928622f1d4298b8a6d
six_xia <feidil@163.com>
2019-01-29 11:56:29 +0800

    share all

---
+++ app/src/main/java/com/wenxi/sompde/MainActivity.java
@@ -0,0 +1,16 @@
+package com.wenxi.sompde;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "congratulation you have successfully connent to de library", Toast.LENGTH_SHORT).show();
+    }
+}
